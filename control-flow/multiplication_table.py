9674ee327cc409d379b451c682d0044ac212fdae
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-11-10 13:20:53 +0200

    changes to print format

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,3 @@
-number=int(input('Enter a number to see its multiplication table: '))
-start=1
-for i in range(1,11):
-    print(f"{number} × {i} = {i*number}")
+number = int(input('Enter a number to see its multiplication table: '))
+for i in range(1, 11):
+    print(f"{number} * {i} = {i * number}")
