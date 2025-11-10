0a29f988dc98f60601c541189f1bba5ba1bfbe2f
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-11-10 12:53:06 +0200

    change for print format

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,11 +1,10 @@
-#conditions: sunny, rainy, or cold.
-weather_condition=input(f' What\'s the weather like today? (sunny/rainy/cold): ')
-if weather_condition== 'sunny':
+# conditions: sunny, rainy, or cold.
+weather_condition = input("What's the weather like today? (sunny/rainy/cold): ")
+if weather_condition == 'sunny':
     print('Wear a t-shirt and sunglasses.')
-elif weather_condition=='rainy':
-    print(f'Don\'t forget your umbrella and a raincoat.')
-elif weather_condition=='cold':
-    print(' Make sure to wear a warm coat and a scarf.')
+elif weather_condition == 'rainy':
+    print('Don\'t forget your umbrella and a raincoat.')
+elif weather_condition == 'cold':
+    print('Make sure to wear a warm coat and a scarf.')
 else:
-    print(f'Sorry, I don\'t have recommendations for this weather.')
-    
+    print('Sorry, I don\'t have recommendations for this weather.')
