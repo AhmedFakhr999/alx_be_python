e743bff45d316cbd059c9feb2c0695078d7f3155
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-11-10 14:38:58 +0200

    changes to pattern drawing

--- control-flow/pattern_drawing.py
+++ control-flow/pattern_drawing.py
@@ -1,5 +1,5 @@
-size=int(input('Enter the size of the pattern: '))
-pattern=size
-while size>0 : 
-    print('*'*pattern) 
-    size-=1
+size = int(input('Enter the size of the pattern: '))
+pattern = size
+while size > 0:
+    print('*' * pattern)  # Use 'pattern' (constant) instead of 'size' (decreasing)
+    size -= 1
