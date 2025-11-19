90edbab1a4a8abbea574136b27915fb74ec4c228
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-11-19 17:07:20 +0200

    mod for function

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,7 @@
-def perform_operation(num1: float, num2: float, operation: str):
+def perform_operation(num1, num2, operation):
+    """
+    Performs basic arithmetic operations using structural pattern matching.
+    """
     match operation:
         case 'add':
             return num1 + num2 
@@ -6,9 +9,14 @@ def perform_operation(num1: float, num2: float, operation: str):
             return num1 - num2 
         case 'multiply': 
             return num1 * num2 
+            
+        # Handles 'divide' with a Guard Clause for zero
         case 'divide' if num2 == 0:
             return 'cannot divide by 0'
+            
+        # Handles 'divide' for all other cases (num2 != 0)
         case 'divide':
             return num1 / num2
+            
         case _:
             return 'Not recognized as operation'
