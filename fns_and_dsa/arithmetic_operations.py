8111780be9f66f8e40865c5bfa9794308f8e33a2
76AHMEDFAKHER <220210469@fa-hists.edu.eg>
2025-11-19 19:49:21 +0200

    last modif

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,14 +1,13 @@
 def perform_operation(num1, num2, operation):
-    match operation:
-        case 'add':
-            return num1 + num2 
-        case 'subtract': 
-            return num1 - num2 
-        case 'multiply': 
-            return num1 * num2 
-        case 'divide':
-            if num2 == 0:
-                return 'cannot divide by 0'
-            return num1 / num2
-        case _:
-            return 'Not recognized as operation'
+    if operation =='add':
+        return num1 + num2 
+    elif operation== 'subtract':
+        return num1 - num2 
+    elif operation =='multiply':
+        return num1*num2 
+    elif operation == 'divide': 
+        if num2== 0 : 
+            return 'cannot divide by 0' 
+        else : 
+            return num1/num2
+        []
